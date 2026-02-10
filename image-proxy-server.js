2d5d328442c4fe197fa02a8c9fa0aa8f98787e4d
Leandro Marques <leandromarques@grupometarh.local>
2026-02-10 11:12:05 -0300

    erro download 5

--- image-proxy-server.js
+++ image-proxy-server.js
@@ -9,20 +9,30 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
 app.get('/api/image-proxy', async (req, res) => {
   let url = req.query.url;
-  if (!url || typeof url !== 'string') return res.status(400).send('Missing url param');
+  if (!url || typeof url !== 'string') {
+    res.header('Access-Control-Allow-Origin', '*');
+    return res.status(400).send('Missing url param');
+  }
   try {
     // Decodifica caso a URL venha codificada
     url = decodeURIComponent(url);
     const response = await fetch(url);
-    if (!response.ok) return res.status(403).send('Image not accessible');
+    if (!response.ok) {
+      res.header('Access-Control-Allow-Origin', '*');
+      return res.status(403).send('Image not accessible');
+    }
     res.set('Content-Type', response.headers.get('content-type'));
     response.body.pipe(res);
   } catch (e) {
+    res.header('Access-Control-Allow-Origin', '*');
     res.status(500).send('Error fetching image');
   }
 });
